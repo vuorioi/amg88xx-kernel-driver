8156f8d80ca7dabd135fac4a638ed2cf7b9b637c
Iiro Vuorio <iiro.vuorio@student.tut.fi>
2018-10-16 14:38:03 +0300

    Fixed some whitespace issues and typos

--- amg88xx.c
+++ amg88xx.c
@@ -32,8 +32,8 @@
 #define THERM_LOW_REG 0x0e
 #define THERM_HIGH_REG 0x0f
 
-#define SENSOR_FIRST_REG
-#define SENSOR_LAST_REG
+#define SENSOR_FIRST_REG 0x80
+#define SENSOR_LAST_REG 0xFF
 
 static inline int amg88xx_write_register(struct i2c_client *client, u8 reg_addr, u8 value)
 {
@@ -53,13 +53,13 @@ static int amg88xx_read16b_register(struct i2c_client *client, u8 regl, u8 regh)
 	int val;
 
 	// First get the high bits and then the low bits
-	ret = amg88xx_read_register(dev->client, regh);
+	ret = amg88xx_read_register(client, regh);
 	if (ret < 0)
 		return ret;
 	else
 		val = ret << 8;
 
-	ret = amg88xx_read_register(dev->client, regl);
+	ret = amg88xx_read_register(client, regl);
 	if (ret < 0)
 		return ret;
 	else
@@ -69,50 +69,50 @@ static int amg88xx_read16b_register(struct i2c_client *client, u8 regl, u8 regh)
 }
 
 enum amg88xx_device_mode {
-        NORMAL_MODE = 0x0,
-        SLEEP_MODE = 0x10,
-        STANDBY60_MODE = 0x20,
-        STANDBY10_MODE = 0x21 };
+	NORMAL_MODE = 0x0,
+	SLEEP_MODE = 0x10,
+	STANDBY60_MODE = 0x20,
+	STANDBY10_MODE = 0x21 };
 
 enum amg88xx_reset_mode {
 	PARTIAL_RST = 0x30,
 	FULL_RST = 0x3f };
 
 enum amg88xx_fps {
-        FPS10 = 0,
-        FPS1 = 1 };
+	FPS10 = 0,
+	FPS1 = 1 };
 
 enum amg88xx_interrupt_mode {
-        DIFFERENCE_MODE = 0,
-        ABSOLUTE_VALUE_MODE = 1 };
+	DIFFERENCE_MODE = 0,
+	ABSOLUTE_VALUE_MODE = 1 };
 
 enum amg88xx_interrupt_state {
-        INT_DISABLED = 0,
-        INT_ENABLED = 1 };
+	INT_DISABLED = 0,
+	INT_ENABLED = 1 };
 
 struct amg88xx {
-        struct i2c_client *client;
+	struct i2c_client *client;
+	
+	s16 temp_array[8*8];
 
-        s16 temp_array[8*8];
-        
-        /*enum amg88xx_device_mode dev_mode;
-        enum amg88xx_fps fps;
+	/*enum amg88xx_device_mode dev_mode;
+	enum amg88xx_fps fps;
 
-        enum amg88xx_interrupt_mode int_mode;
-        enum amg88xx_interrupt_state int_sate;
-        u8 interrupt_array[8];
+	enum amg88xx_interrupt_mode int_mode;
+	enum amg88xx_interrupt_state int_sate;
+	u8 interrupt_array[8];
 
-        s16 int_trigger_level_high : 11;
-        s16 int_trigger_level_low : 11;
-        s16 int_trigger_hysteresis : 11;
+	s16 int_trigger_level_high : 11;
+	s16 int_trigger_level_low : 11;
+	s16 int_trigger_hysteresis : 11;
 
-        u8 thermistor_overflow : 1;
-        u8 sensor_overflow : 1;
-        u8 interrupt_active : 1;
+	u8 thermistor_overflow : 1;
+	u8 sensor_overflow : 1;
+	u8 interrupt_active : 1;
 
-        u8 twice_moving_average_mode : 1;*/
+	u8 twice_moving_average_mode : 1;*/
 
-        s16 thermistor_value : 11;
+	s16 thermistor_value : 11;
 };
 
 static inline int amg88xx_set_dev_mode(struct amg88xx *dev, enum amg88xx_device_mode mode)
@@ -146,6 +146,9 @@ static inline int amg88xx_read_thermistor(struct amg88xx *dev)
 
 static int amg88xx_read_sensor(struct amg88xx *dev)
 {
+	int row;
+	int col;
+	int ret;
 	u8 reg_addr = SENSOR_FIRST_REG;
 
 	// Loop through all the sensor registers
@@ -166,7 +169,7 @@ static int amg88xx_read_sensor(struct amg88xx *dev)
 
 /* sysfs entries */
 static ssize_t show_sensor(struct device *dev, struct device_attribute *attr,
-			   const char *buf)
+			   char *buf)
 {
 	struct amg88xx *device;
 	int row;
@@ -180,10 +183,10 @@ static ssize_t show_sensor(struct device *dev, struct device_attribute *attr,
 		for (col = 0; col < 8; col++) {
 			/* Write all the values on a row. Each value is sepparated by a comma
 			   and there is newline character after the last value */
-			nwrite = scprintf(&buf[index],
-					  PAGE_SIZE - (index - 1),
-					  col < 7 ? "%d, " : "%d\n",
-					  (int)device->temp_array[row*8 + col]);
+			nwrite = scnprintf(&buf[index],
+					   PAGE_SIZE - (index - 1),
+					   col < 7 ? "%d, " : "%d\n",
+					   (int)device->temp_array[row*8 + col]);
 			index += nwrite + 1;
 		}
 	}
@@ -193,7 +196,7 @@ static ssize_t show_sensor(struct device *dev, struct device_attribute *attr,
 static DEVICE_ATTR(sensor, S_IRUGO, show_sensor, NULL);
 
 static ssize_t show_thermistor(struct device *dev, struct device_attribute *attr,
-			       const char *buf)
+			       char *buf)
 {
 	struct amg88xx *device;
 
@@ -204,28 +207,27 @@ static DEVICE_ATTR(thermistor, S_IRUGO, show_thermistor, NULL);
 
 static int amg88xx_probe_new(struct i2c_client *client)
 {
-        int ret;
-	int temperature;
-        struct amg88xx *device;
+	int ret;
+	struct amg88xx *device;
 
-        device = devm_kzalloc(&client->dev, sizeof(*device), GFP_KERNEL);
+	device = devm_kzalloc(&client->dev, sizeof(*device), GFP_KERNEL);
 
-        if (device == NULL)
-                return -ENOMEM;
-        else
-                device->client = client;
+	if (device == NULL)
+		return -ENOMEM;
+	else
+		device->client = client;
 
 	dev_set_drvdata(&client->dev, device);
-        
-        ret = amg88xx_get_dev_mode(device);
+
+	ret = amg88xx_get_dev_mode(device);
 	if (ret < 0) {
 		printk(KERN_ERR "Failed to read device mode\n");
 		return ret;
 	} else if (!(ret == NORMAL_MODE ||
 		   ret == SLEEP_MODE ||
 		   ret == STANDBY60_MODE ||
-		   ret == STANDBY11_MODE)) {
-                return -EPROTO;
+		   ret == STANDBY10_MODE)) {
+		return -EPROTO;
 	}
 
         ret = amg88xx_reset(device);
@@ -233,58 +235,69 @@ static int amg88xx_probe_new(struct i2c_client *client)
 		printk(KERN_ERR "Failed to reset device\n");
 		return ret;
 	}
-        
+
 	// Start the device
-        ret = amg88xx_set_dev_mode(device, NORMAL_MODE);
+	ret = amg88xx_set_dev_mode(device, NORMAL_MODE);
 	if (ret < 0) {
 		printk(KERN_ERR "Failed to set device mode\n");
 		return ret;
 	}
 
-        ret = amg88xx_read_thermistor(device);
+	ret = amg88xx_read_thermistor(device);
 	if (ret < 0) {
 		printk(KERN_ERR "Failed to read thermistor value\n");
 		return ret;
 	}
 
 	printk(KERN_INFO "thermistor value: %d\n", device->thermistor_value);
+	
+	// TODO create sysfs entries
+	device_create_file(&client->dev, &dev_attr_sensor);
+	device_create_file(&client->dev, &dev_attr_thermistor);
 
-        // TODO create sysfs entries
-	device_create_file(&client->dev, &device_attr_sensor);
-	device_create_file(&client->dev, &device_attr_thermistor);
+	return 0;
 }
 
 static int amg88xx_remove(struct i2c_client *client)
 {
-        amg88xx_set_dev_mode(&client->dev, SLEEP_MODE);
+	int ret;
+	struct amg88xx *device = dev_get_drvdata(&client->dev);
 
-        // TODO clear sysfs entries
-	device_remove_file(&client->dev, &device_attr_sensor);
-	device_remove_file(&client->dev, &device_attr_thermistor);
+	ret = amg88xx_set_dev_mode(device, SLEEP_MODE);
+	if (ret < 0) {
+		printk(KERN_ERR "Failed to put the device to sleep\n");
+		return ret;
+	}
+
+	// TODO clear sysfs entries
+	device_remove_file(&client->dev, &dev_attr_sensor);
+	device_remove_file(&client->dev, &dev_attr_thermistor);
+
+	return 0;
 }
 
 
 static const struct i2c_device_id amg88xx_id_table[] = {
-        { "amg88xx", 0 },
-        {},
+	{ "amg88xx", 0 },
+	{},
 };
 MODULE_DEVICE_TABLE(i2c, amg88xx_id_table);
 
 static const struct of_device_id amg88xx_of_match[] = {
-         { .compatible = "panasonic,amg88xx" },
-         {},
+	{ .compatible = "panasonic,amg88xx" },
+	{},
 };
 MODULE_DEVICE_TABLE(of, amg88xx_of_match);
 
 static struct i2c_driver amg88xx_driver = {
-        .driver = {
-                .owner          = THIS_MODULE,
-                .name           = DRIVER_NAME,
-                .of_match_table = of_match_ptr(amg88xx_of_match),
-        },
-        .probe_new  = amg88xx_probe_new,
-        .remove     = amg88xx_remove,
-        .id_table   = amg88xx_id,
+	.driver = {
+		.owner		= THIS_MODULE,
+		.name		= DRIVER_NAME,
+		.of_match_table	= of_match_ptr(amg88xx_of_match),
+	},
+	.probe_new = amg88xx_probe_new,
+	.remove	   = amg88xx_remove,
+	.id_table  = amg88xx_id_table,
 };
 
 static int __init amg88xx_module_init(void)
@@ -301,5 +314,5 @@ module_exit(amg88xx_module_exit);
 
 MODULE_VERSION("1.0");
 MODULE_AUTHOR("Iiro Vuorio");
-MODULE_LISENCE("GPL v2");
+MODULE_LICENSE("GPL v2");
 MODULE_DESCRIPTION("A kernel driver for the Panasonic AMG88xx-series sensors.");
