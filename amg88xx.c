351cd00894adada4563b9f5d54f7b05eb91086ef
Iiro Vuorio <iiro.vuorio@student.tut.fi>
2018-10-16 17:44:08 +0300

    Added sysfs interfaces and changed some implementation details

--- amg88xx.c
+++ amg88xx.c
@@ -27,23 +27,39 @@
 #define DRIVER_NAME "amg88xx"
 
 /* i2c register addresses */
-#define MODE_REG 0x00
+#define DEVICE_MODE_REG 0x00
 #define RESET_REG 0x01
+#define FRAME_RATE_REG 0x02
+#define INTERRUPT_MODE_REG 0x03
+#define XXX_REG 0x04
+#define MOVING_AVERAGE_REG 0x07
+#define UPPER_INTERRUPT_LOW_REG 0x08
+#define UPPER_INTERRUPT_HIGH_REG 0x09
+#define LOWER_INTERRUPT_LOW_REG 0x0a
+#define LOWER_INTERRUPT_HIGH_REG 0x0b
+#define INTERRUPT_HYST_LOW_REG 0x0c
+#define INTERRUPT_HYST_HIGH_REG 0x0d
 #define THERM_LOW_REG 0x0e
 #define THERM_HIGH_REG 0x0f
+#define PIXEL_ROW1_REG 0x10
+#define PIXEL_ROW2_REG 0x11
+#define PIXEL_ROW3_REG 0x12
+#define PIXEL_ROW4_REG 0x13
+#define PIXEL_ROW5_REG 0x14
+#define PIXEL_ROW6_REG 0x15
+#define PIXEL_ROW7_REG 0x16
+#define PIXEL_ROW8_REG 0x17
+#define SENSOR_FIRST_REG 0x80 // Pixel 1 low bits register
+#define SENSOR_LAST_REG 0xFF // Pixel 64 high bits register
 
-#define SENSOR_FIRST_REG 0x80
-#define SENSOR_LAST_REG 0xFF
-
+/* Low level access helper functions */
 static inline int amg88xx_write_register(struct i2c_client *client, u8 reg_addr, u8 value)
 {
-	//TODO maybe check if register is valid
 	return i2c_smbus_write_byte_data(client, reg_addr, value);
 }
 
 static inline int amg88xx_read_register(struct i2c_client *client, u8 reg_addr)
 {
-	//TODO maybe check if register is valid
 	return i2c_smbus_read_byte_data(client, reg_addr);
 }
 
@@ -68,6 +84,7 @@ static int amg88xx_read16b_register(struct i2c_client *client, u8 regl, u8 regh)
 	return val;
 }
 
+/* Device configuration options that are mapped to register values */
 enum amg88xx_device_mode {
 	NORMAL_MODE = 0x0,
 	SLEEP_MODE = 0x10,
@@ -90,12 +107,13 @@ enum amg88xx_interrupt_state {
 	INT_DISABLED = 0,
 	INT_ENABLED = 1 };
 
+/* Structure for holding device related data */
 struct amg88xx {
 	struct i2c_client *client;
 	
-	s16 temp_array[8*8];
+	/*s16 temp_array[8*8];
 
-	/*enum amg88xx_device_mode dev_mode;
+	enum amg88xx_device_mode dev_mode;
 	enum amg88xx_fps fps;
 
 	enum amg88xx_interrupt_mode int_mode;
@@ -110,74 +128,87 @@ struct amg88xx {
 	u8 sensor_overflow : 1;
 	u8 interrupt_active : 1;
 
-	u8 twice_moving_average_mode : 1;*/
+	u8 twice_moving_average_mode : 1;
 
-	s16 thermistor_value : 11;
+	s16 thermistor_value : 11;*/
 };
 
+/* Helper functions for device access */
 static inline int amg88xx_set_dev_mode(struct amg88xx *dev, enum amg88xx_device_mode mode)
 {
-	return amg88xx_write_register(dev->client, MODE_REG, mode);
+	return amg88xx_write_register(dev->client, DEVICE_MODE_REG, mode);
 }
 
-static inline int amg88xx_get_dev_mode(struct amg88xx *dev)
+static int amg88xx_get_dev_mode(struct amg88xx *dev, int *result)
 {
-	return amg88xx_read_register(dev->client, MODE_REG);
+	int ret;
+
+	ret = amg88xx_read_register(dev->client, DEVICE_MODE_REG);
+	if (ret < 0)
+		return ret;
+	else
+		*result = ret;
+
+	return 0;
 }
 
 static inline int amg88xx_reset(struct amg88xx *dev)
 {
+	// Performe a "soft reset"
 	return amg88xx_write_register(dev->client, RESET_REG, PARTIAL_RST);
 }
 
-static inline int amg88xx_read_thermistor(struct amg88xx *dev)
+static int amg88xx_read_thermistor(struct amg88xx *dev, int *result)
 {
 	int ret;
 
 	ret = amg88xx_read16b_register(dev->client, THERM_LOW_REG, THERM_HIGH_REG);
-	if (ret < 0) {
-		printk(KERN_ERR "Failed to read the thermistor registers\n");
+	if (ret < 0) 
 		return ret;
-	}
+	else
+		*result = ret;
 
-	dev->thermistor_value = (s16)ret;
 	return 0;
 }
 
-static int amg88xx_read_sensor(struct amg88xx *dev)
+static int amg88xx_read_sensor(struct amg88xx *dev, int *res_array)
 {
-	int row;
-	int col;
+	int index;
 	int ret;
 	u8 reg_addr = SENSOR_FIRST_REG;
 
 	// Loop through all the sensor registers
-	for (row = 0; row < 8; row++) {
-		for (col = 0; col < 8; col++) {
-			ret = amg88xx_read16b_register(dev->client, reg_addr, reg_addr + 1);
-			if (ret < 0) {
-				printk(KERN_ERR "Failed to read sensor value at: %d.%d\n",
-						col, row);
-				return ret;
-			}
+	for (index = 0; index < 64; index++) {
+		ret = amg88xx_read16b_register(dev->client, reg_addr, reg_addr + 1);
+		if (ret < 0)
+			return ret;
 
-			dev->temp_array[row*8 + col] = (s16)ret;
-			reg_addr += 2;
-		}
+		res_array[index] = ret;
+		reg_addr += 2;
 	}
+
+	return 0;
 }
 
-/* sysfs entries */
+/* sysfs entries and the functions to implement them */
 static ssize_t show_sensor(struct device *dev, struct device_attribute *attr,
 			   char *buf)
 {
 	struct amg88xx *device;
+	int ret;
 	int row;
 	int col;
 	int nwrite;
+	int sensor_array[64];
 	unsigned index = 0;
 
 	device = dev_get_drvdata(dev);
+
+	ret = amg88xx_read_sensor(device, sensor_array);
+	if (ret < 0) {
+		printk(KERN_ERR "Failed to read the sensor\n");
+		return ret;
+	}
 
 	for (row = 0; row < 8; row++) {
 		for (col = 0; col < 8; col++) {
@@ -186,7 +217,7 @@ static ssize_t show_sensor(struct device *dev, struct device_attribute *attr,
 			nwrite = scnprintf(&buf[index],
 					   PAGE_SIZE - (index - 1),
 					   col < 7 ? "%d, " : "%d\n",
-					   (int)device->temp_array[row*8 + col]);
+					   sensor_array[row*8 + col]);
 			index += nwrite + 1;
 		}
 	}
@@ -199,11 +230,78 @@ static ssize_t show_thermistor(struct device *dev, struct device_attribute *attr
 			       char *buf)
 {
 	struct amg88xx *device;
+	int ret;
+	int thermistor_value;
 
 	device = dev_get_drvdata(dev);
-	return scnprintf(buf, PAGE_SIZE, "%d\n", (int)device->thermistor_value);
+
+	ret = amg88xx_read_thermistor(device, &thermistor_value);
+	if (ret < 0) {
+		printk(KERN_ERR "Failed to read thermistor value\n");
+		return ret;
+	}
+
+	return scnprintf(buf, PAGE_SIZE, "%d\n", thermistor_value);
 }
 static DEVICE_ATTR(thermistor, S_IRUGO, show_thermistor, NULL);
+
+static ssize_t show_device_mode(struct device *dev, struct device_attribute *attr,
+				char *buf)
+{
+	struct amg88xx *device;
+	int ret;
+	int device_mode;
+
+	device = dev_get_drvdata(dev);
+
+	ret = amg88xx_get_dev_mode(device, &device_mode);
+	if (ret < 0) {
+		printk(KERN_ERR "Failed to read device mode\n");
+		return ret;
+	}
+
+	return scnprintf(buf, PAGE_SIZE, "%x\n", device_mode);
+}
+
+static ssize_t store_device_mode(struct device *dev, struct device_attribute *attr,
+				 const char *buf, size_t count)
+{
+	struct amg88xx *device;
+	int ret;
+	u8 mode;
+
+	device = dev_get_drvdata(dev);
+
+	ret = kstrtou8(buf, 16, &mode);
+	if (ret < 0) {
+		printk(KERN_ERR "Failed to read value from input\n");
+		return ret;
+	}
+
+	if (!(mode == NORMAL_MODE ||
+	      mode == SLEEP_MODE ||
+	      mode == STANDBY60_MODE ||
+	      mode == STANDBY10_MODE)) {
+		printk(KERN_ERR "Input is not a supported mode\n");
+		return -EINVAL;
+	}
+
+	ret = amg88xx_set_dev_mode(device, mode);
+	if (ret < 0) {
+		printk(KERN_ERR "Failed to set device mode\n");
+		return ret;
+	}
+
+	return count;
+}
+
+static DEVICE_ATTR(device_mode,
+		   S_IRUGO | S_IWUSR | S_IWGRP,
+		   show_device_mode,
+		   store_device_mode);
+
+// TODO all the rest of the sysfs stuff
+// TODO group attributes
 
 static int amg88xx_probe_new(struct i2c_client *client)
 {
@@ -219,41 +317,17 @@ static int amg88xx_probe_new(struct i2c_client *client)
 
 	dev_set_drvdata(&client->dev, device);
 
-	ret = amg88xx_get_dev_mode(device);
-	if (ret < 0) {
-		printk(KERN_ERR "Failed to read device mode\n");
-		return ret;
-	} else if (!(ret == NORMAL_MODE ||
-		   ret == SLEEP_MODE ||
-		   ret == STANDBY60_MODE ||
-		   ret == STANDBY10_MODE)) {
-		return -EPROTO;
-	}
-
         ret = amg88xx_reset(device);
 	if (ret < 0) {
 		printk(KERN_ERR "Failed to reset device\n");
 		return ret;
 	}
 
-	// Start the device
-	ret = amg88xx_set_dev_mode(device, NORMAL_MODE);
-	if (ret < 0) {
-		printk(KERN_ERR "Failed to set device mode\n");
-		return ret;
-	}
-
-	ret = amg88xx_read_thermistor(device);
-	if (ret < 0) {
-		printk(KERN_ERR "Failed to read thermistor value\n");
-		return ret;
-	}
-
-	printk(KERN_INFO "thermistor value: %d\n", device->thermistor_value);
-	
 	// TODO create sysfs entries
+	// TODO move sysfs entries to attribute group
 	device_create_file(&client->dev, &dev_attr_sensor);
 	device_create_file(&client->dev, &dev_attr_thermistor);
+	device_create_file(&client->dev, &dev_attr_device_mode);
 
 	return 0;
 }
@@ -263,15 +337,16 @@ static int amg88xx_remove(struct i2c_client *client)
 	int ret;
 	struct amg88xx *device = dev_get_drvdata(&client->dev);
 
+	// TODO clear sysfs entries
+	device_remove_file(&client->dev, &dev_attr_sensor);
+	device_remove_file(&client->dev, &dev_attr_thermistor);
+	device_remove_file(&client->dev, &dev_attr_device_mode);
+
 	ret = amg88xx_set_dev_mode(device, SLEEP_MODE);
 	if (ret < 0) {
 		printk(KERN_ERR "Failed to put the device to sleep\n");
 		return ret;
 	}
-
-	// TODO clear sysfs entries
-	device_remove_file(&client->dev, &dev_attr_sensor);
-	device_remove_file(&client->dev, &dev_attr_thermistor);
 
 	return 0;
 }
